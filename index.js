ebaa25f7d1b0b3afc7ebf199ed73665a7401b714
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-06-06 15:24:47 -0700

    added pokemon move info

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const results = document.querySelector('.results');
+const results = document.querySelector('.name-display');
+const resultsContainer = document.querySelector('.results-container');
 
 async function fetchData(searchTerm) {
     let address = 'https://pokeapi.co/api/v2/pokemon/' + searchTerm;
     const response = await axios.get(address);
-    console.log("searchTerm is: ", searchTerm);
-    console.log("address is: ", address);
+
+    console.log(response.data);
+
     if (response.data.error) {
         return [];
-    } else {
-    results.innerHTML = `${response.data.name}`
-    const leftImg = document.querySelector('#left-img-container');
-    leftImg.innerHTML = `<img class="sprite-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${response.data.id}.png" />`
-    console.log(response.data);
-    return response.data;
+    } else if (!response.data.previous) {
+        resultsContainer.innerHTML = '';
+    }
+    else {
+        results.innerHTML = `${response.data.name}`
+        const imgContainer = document.querySelector('#img-container');
+        const column2 = document.querySelector('.column2');
+        imgContainer.innerHTML = `<img class="sprite-img image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${response.data.id}.png" />`;
+        column2.innerHTML = `<p class="col2-info" >${response.data.moves[0].move.name}</p>`
+        console.log(response.data);
+        return response.data;
     }
 };
 
