1c306e42cb70b1a8af2710ddfc6c6780ba1d3bae
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-06-30 20:10:00 -0700

    pokedex update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const results = document.querySelector('.name-display');
 const resultsContainer = document.querySelector('.results-container');
+const card = document.querySelector('#card');
 
 async function fetchData(searchTerm) {
     let address = 'https://pokeapi.co/api/v2/pokemon/' + searchTerm;
@@ -13,12 +14,14 @@ async function fetchData(searchTerm) {
         resultsContainer.innerHTML = '';
     }
     else {
-        results.innerHTML = `${response.data.name}`
+        results.innerHTML = `${response.data.name}`;
+        console.log(response.data.types[0].type.name);
         const imgContainer = document.querySelector('#img-container');
         const column2 = document.querySelector('.column2');
         imgContainer.innerHTML = `<img class="sprite-img image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${response.data.id}.png" />`;
         column2.innerHTML = `<p class="col2-info">${response.data.moves[0].move.name}</p>`
         console.log(response.data);
+        card.classList.add("border-yellow-400");
         return response.data;
     }
 };
