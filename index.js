e7d4869ab3bedf63d106234670f6f2981e81784c
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-06-30 21:58:40 -0700

    layout redone

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const results = document.querySelector('.name-display');
 const resultsContainer = document.querySelector('.results-container');
+const imgContainer = document.querySelector('#img-container');
+const column2 = document.querySelector('.column2');
 const card = document.querySelector('#card');
 
 async function fetchData(searchTerm) {
@@ -16,12 +18,49 @@ async function fetchData(searchTerm) {
     else {
         results.innerHTML = `${response.data.name}`;
         console.log(response.data.types[0].type.name);
-        const imgContainer = document.querySelector('#img-container');
-        const column2 = document.querySelector('.column2');
+        const type = response.data.types[0].type.name;
+        switch (type) {
+            case "fire":
+                card.classList.add("bg-red-600");
+                break;
+            case "water":
+                card.classList.add("bg-sky-400");
+                break;
+            case "grass":
+                card.classList.add("bg-green-500");
+                break;
+            case "bug":
+                card.classList.add("bg-red-600");
+                break;
+            case "electric":
+                card.classList.add("bg-amber-300");
+                break;
+            case "psychic":
+                card.classList.add("bg-violet-800");
+                break;
+            case "ground":
+                card.classList.add("bg-yellow-700");
+                break;
+            case "ice":
+                card.classList.add("bg-cyan-200");
+                break;
+            case "dark":
+                card.classList.add("bg-indigo-900");
+                break;
+            case "fairy":
+                card.classList.add("bg-pink-300");
+                break;
+            case "normal":
+                card.classList.add("bg-yellow-200");
+                break;
+            case "fighting":
+                card.classList.add("bg-amber-600");
+                break;
+            default: 
+                card.classList.add("bg-zinc-500");
+        }
         imgContainer.innerHTML = `<img class="sprite-img image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${response.data.id}.png" />`;
         column2.innerHTML = `<p class="col2-info">${response.data.moves[0].move.name}</p>`
-        console.log(response.data);
-        card.classList.add("border-yellow-400");
         return response.data;
     }
 };
